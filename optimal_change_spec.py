8e49cb45fa0d0ce8ff2569f060dcd1c7183acc88
jakewhite5 <jakewhite5@yahoo.com>
2021-06-04 14:36:23 -0700

    final push

--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -21,9 +21,9 @@ class TestMode(unittest.TestCase):
     def test_for_refund(self):
         self.assertEqual(optimal_change(-80, 50), 'Vending machines do not offer refunds at this time.')
 
-    
     def test_for_primary_function(self):
          self.assertEqual(optimal_change(62.13, 100), "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
+
     def test_for_primary_function2(self):
         self.assertEqual(optimal_change(31.51, 50), "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
 
