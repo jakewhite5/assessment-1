94b43f176a67458782c9c657531947552e772239
jakewhite5 <jakewhite5@yahoo.com>
2021-06-04 11:05:14 -0700

    Completed with unittesting

--- optimal_change.py
+++ optimal_change.py
@@ -1,1 +1,45 @@
-# Write your solution here!
+def optimal_change(price, given):
+    # determines if both items are numbers and returns error if not
+    try:
+        # determines amount of change needed to be returned
+        change_amount = given - price
+    except:
+        return 'Both inputs must be a number.'
+    if change_amount == 0:
+        return 'No change needed.'
+    if change_amount < 0:
+        return 'Insuffient funds.'
+    return_string = ''
+    #holding/staging area to collect amounts of each denomination of change
+    count_dict = {}
+    value_dict = {'$100 bill': 100, '$50 bill':50, '$20 bill':20, '$10 bill':10, '$5 bill':5, '$1 bill':1, 'quarter':.25, 'dime':.1, 'nickel':.05, 'penny':.01}
+    # loop that compares change_amount to each denomination and pushes the amount of each needed to count_dict
+    for key in value_dict:
+        while change_amount >= value_dict[key]:
+            change_amount -= value_dict[key]
+            change_amount = round(change_amount, 2)
+            # checking whether to increment count_dict or add new entry
+            if key in count_dict:
+                count_dict[key] = (count_dict[key] + 1)
+            else:
+                count_dict[key] = 1
+    # creating dynamic portion of the final return string. Determines punctuation, plurality and whether the word needs to be proceeded with 'and'            
+    for item in count_dict:
+        if item == list(count_dict.keys())[-1] and (len(list(count_dict)) > 1):
+            return_string += ' and'
+        # penny has a special case for plurality
+        if item == 'penny' and count_dict[item] > 1:
+            return_string += ' '+str(count_dict[item])+' pennies '
+        # adds 's' to non-penny plural items 
+        elif count_dict[item] > 1:
+            return_string += ' '+str(count_dict[item])+' '+item+'s,'
+        else:
+            return_string += ' '+str(count_dict[item])+' '+item+','
+ 
+
+    return (f'The optimal change for an item that costs ${price} with an amount paid of ${given} is{return_string[:-1]}.')
+
+
+optimal_change('a', 'b')
+
+
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,1 +1,29 @@
-# Write your unit tests here!
+import unittest
+from optimal_change import optimal_change
+
+class TestMode(unittest.TestCase):
+
+    def test_for_correct_answer_type(self):
+        self.assertIsInstance(optimal_change(50, 100), str)
+
+    def test_for_bad_input(self):
+        self.assertEqual(optimal_change('A', 100), 'Both inputs must be a number.')
+    
+    def test_for_no_change(self):
+        self.assertEqual(optimal_change(100, 100), 'No change needed.')
+    
+    def test_for_insufficient_funds(self):
+        self.assertEqual(optimal_change(80, 50), 'Insuffient funds.')
+    
+    def test_for_primary_function(self):
+         self.assertEqual(optimal_change(62.13, 100), "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
+    def test_for_primary_function2(self):
+        self.assertEqual(optimal_change(31.51, 50), "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
+    def test_for_primary_function2(self):
+        self.assertEqual(optimal_change(90, 100), 'The optimal change for an item that costs $90 with an amount paid of $100 is 1 $10 bill.')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
